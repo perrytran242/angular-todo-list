ba4ae46797590348f0c0e8e1f1bbff39cf9d9abc
perrytran242 <ptran209@gmail.com>
2019-12-04 20:48:09 -0800

    added ngx modal in todo item

--- src/app/todolist/todoitem/todoitem.component.ts
+++ src/app/todolist/todoitem/todoitem.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, Input, Output, EventEmitter, OnChanges } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, OnChanges, TemplateRef } from '@angular/core';
 import { TodoService } from 'src/app/services/todo.service';
 import { Router } from '@angular/router';
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 
 
 @Component({
@@ -12,16 +13,20 @@ export class TodoitemComponent implements OnInit, OnChanges {
   @Input() todoItem: any;
   @Output() completedTodoItem = new EventEmitter();
   @Output() deleteTodoItem = new EventEmitter();
+
+
+  modalRef: BsModalRef;
   completeTodoItem = false;
   completed = false;
 
   constructor(
     private todoService: TodoService,
-    private router: Router
+    private router: Router,
+    private modalService: BsModalService
   ) { }
 
   ngOnInit() {
-    // random text here to commit test.
+
   }
 
   ngOnChanges(changes) { }
@@ -39,5 +44,9 @@ export class TodoitemComponent implements OnInit, OnChanges {
   navigateToTodoInfo() {
     this.router.navigate([`todo-list/${this.todoItem.id}`]);
 
+  }
+
+  onOpenDeleteModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template);
   }
 }
